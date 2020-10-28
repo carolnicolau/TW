891c4fad0820a422943b0ff90cc949e46dd32a70
Carolina Nicolau <up201605242@fc.up.pt>
2020-10-28 17:19:12 +0000

    criar tabuleiro

--- game.js
+++ game.js
@@ -1,51 +1,53 @@
-var janela = document.getElementsByClassName('janela');
-// Get the button that opens the modal
-var botao = document.getElementsByClassName("botaojanela");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close");
+window.onload = function() {
 
-// sim funciona When the user clicks the button, open the modal 
-botao[0].onclick = function() {
-  janela[0].style.display = "block";
-}
-//extra n descrito ??
-botao[1].onclick = function() {
-  janela[1].style.display = "block";
-} 
+  var janela = document.getElementsByClassName('janela');
+  // Get the button that opens the modal
+  var botao = document.getElementsByClassName("botaojanela");
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close");
 
-// sim funciona When the user clicks on <span> (x), close the modal
-span[0].onclick = function() {
-  janela[0].style.display = "none";
-}
-
-//??
-span[1].onclick = function() {
-  janela[1].style.display = "none";
-}
-// isto n funciona pq tá bloqueado When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == janela) {
-    janela.style.display = "none";
+  // sim funciona When the user clicks the button, open the modal 
+  botao[0].onclick = function() {
+    janela[0].style.display = "block";
   }
-}
-//FIM DA NAVBAR
+  //extra n descrito ??
+  botao[1].onclick = function() {
+    janela[1].style.display = "block";
+  } 
 
-var oponente="";
+  // sim funciona When the user clicks on <span> (x), close the modal
+  span[0].onclick = function() {
+    janela[0].style.display = "none";
+  }
 
-function selectOponente(){
+  //??
+  span[1].onclick = function() {
+    janela[1].style.display = "none";
+  }
+  // isto n funciona pq tá bloqueado When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == janela) {
+      janela.style.display = "none";
+    }
+  }
+  //FIM DA NAVBAR
 
-  if(document.getElementById("computador").checked){
-   oponente="Computador";
+  var oponente="";
+
+  function selectOponente(){
+
+    if(document.getElementById("computador").checked){
+     oponente="Computador";
+   }
+   else (document.getElementById("jogador").checked){
+     oponente="Jogador";
+   }
+
  }
- else (document.getElementById("jogador").checked){
-   oponente="Jogador";
- }
 
-}
+ var cor="";
 
-var cor="";
-
-document.getElementById("preto").onclick = function() {
+ document.getElementById("preto").onclick = function() {
   cor=(document.getElementById("preto").value);
 } 
 
@@ -56,30 +58,57 @@ document.getElementById("branco").onclick = function() {
 var dificuldade="";
 
 function selectDificuldade(){
-	dificuldade=(document.getElementById("Dificuldade").value)
+ dificuldade=(document.getElementById("Dificuldade").value)
 }
 
-//ACCIONAR BOTÃO DE INICIAR
-document.getElementById("iniciar").onclick = function() {
+  //ACCIONAR BOTÃO DE INICIAR
+  document.getElementById("iniciar").onclick = function() {
 
-	
-  if(cor=="" && oponente == "") {
-    alert("Selecione as configurações");
-  }
-  else {
+  	
+    if(cor=="" && oponente == "") {
+      alert("Selecione as configurações");
+    }
+    else {
+        const base = document.getElementById("base");
+        const tabul = document.createElement("div");
 
-    alert("GERAR TABULEIRO");
+        tabul.className = "tabuleiro";
+        base.id ="base";
+        base.appendChild(tabul);
+      //alert("GERAR TABULEIRO");
+    }
   }
+
+
+  /*
+  //Gerar Tabuleiro
+  const tabuleiro = new Tabuleiro("base"); //chamar só depois de selecionar configs?
   
+  class Tabuleiro {
+
+   constructor(id_base) {
+
+    const base = document.getElementById(id_base);
+    const tabul = document.createElement("div");
+
+    tabul.className = "tabuleiro";
+    tabul.id = "tabul";
+    base.appendChild(tabul);
+    
+
+    /*
+    for(let l = 0; l<8; l++) {
+      const linha = document.createElement("div");
+      for(let c=0; c<8; c++) {
+        const celula = document.createElement("div");
+      }
+    }
+
+    parent.appendChild(counter);
+    counter.appendChild(incr);
+    counter.appendChild(reset);
+    counter.appendChild(this.display);
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
+*/
+}
