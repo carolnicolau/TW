302a9bfd5a053b31c52c581d83cfe838b897938b
carolnicolau <62442865+carolnicolau@users.noreply.github.com>
2020-10-31 10:51:16 +0000

    Add files via upload

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 window.onload = function() {
 
+  var jogo;
+  
   var janela = document.getElementsByClassName('janela');
   // Get the button that opens the modal
   var botao = document.getElementsByClassName("botaojanela");
@@ -47,17 +49,13 @@ window.onload = function() {
  }
 
   document.getElementById("preto").onclick = function() {
-    alert("preto!");
     cor=(document.getElementById("preto").value);
     alert("cor = "+cor);
-    //vez='P';
   } 
 
   document.getElementById("branco").onclick = function() {
-    alert("branco!");
     cor=(document.getElementById("branco").value);
     alert("cor = "+cor);
-    //vez='B';
   }
 
   function selectDificuldade(){
@@ -73,7 +71,6 @@ window.onload = function() {
     }
   }
 
-
   function desistir() {
     alert("ganhou alguém");
   }
@@ -86,18 +83,65 @@ window.onload = function() {
   }
   //ACCIONAR BOTÃO DE INICIAR
   document.getElementById("iniciar").onclick = function() {
-    var jogo = new Jogo();
+    jogo = new Jogo();
     escondeEsconde();
+    if((cor=="Branco" && vez=='P') || (cor=="Preto" && vez=='B'))
+      computador();
+  }
+
+  function soma_pecas() {
+      let conta_pretas=0;
+      let conta_brancas=0;
+      let conta_livres=0;
+
+      for(let l = 0; l<8; l++) {
+        for(let c=0; c<8; c++) {
+          if(jogo.conteudo[l][c] == 'B')
+            conta_brancas ++;
+          else if(jogo.conteudo[l][c] == 'P')
+            conta_pretas ++;
+          else
+            conta_livres ++;
+        }
+      }
+      document.getElementById("n_brancas").innerText = conta_brancas;
+      document.getElementById("n_pretas").innerText = conta_pretas;
+      document.getElementById("n_livres").innerText = conta_livres;
+    }
+
+  function computador() {
+    var peca1 = jogo.tabuleiro[1][1].firstChild;
+
+    if(vez=='P') {
+      peca1.className = "peca preto";
+      jogo.conteudo[1][1] = 'P';
+    } else {
+      peca1.className = "peca branco";
+      jogo.conteudo[1][1] = 'B';
+    }
+    MudarDeVez();
   }
 
   class Jogo { 
-    
     play(l, c) {
-      var peca1 = this.tabuleiro[l][c].firstChild;
-      if(vez=='P') {
-        peca1.className= "peca preto";
+      alert("cor = "+cor);
+      if((vez=='P' && cor=="Preto") || (vez=='B' && cor=="Branco")) {
+        var peca1 = this.tabuleiro[l][c].firstChild;
+        
+        if(this.conteudo[l][c] = 'L'){ //verificação passa para jogada válida
+          if(vez=='P') {
+            peca1.className = "peca preto";
+            this.conteudo[l][c] = 'P';
+          } else {
+            peca1.className = "peca branco";
+            this.conteudo[l][c] = 'B';
+          }
+          soma_pecas();
+          MudarDeVez();
+          computador();
+        }
       } else {
-        peca1.className= "peca branco";
+        alert("Não é a sua vez de jogar!");
       }
     }
 
@@ -109,6 +153,12 @@ window.onload = function() {
         this.conteudo = new Array(8);
         this.tabuleiro = new Array(8);
 
+        /*
+        oponente="";
+        vez = 'P';
+        cor="Preto";
+        dificuldade="";
+        */
         const base = document.getElementById("base");
         const tabul = document.createElement("div");
         const passar = document.createElement("button");
@@ -150,26 +200,26 @@ window.onload = function() {
 
             if((l==3 & c==3) || (l==4 & c==4)) {
               peca.className="peca preto";            
-              this.conteudo[l][c] = "P"; 
+              this.conteudo[l][c] = 'P'; 
 
             }
             else if((l==3 & c==4) || (l==4 & c==3)) {
               peca.className="peca branco";
-              this.conteudo[l][c] = "B"; 
+              this.conteudo[l][c] = 'B'; 
 
             }
             else {
               peca.className="peca livre";
-              this.conteudo[l][c] = "L"; 
+              this.conteudo[l][c] = 'L'; 
 
             }
             celula.appendChild(peca);
             
-            if((vez=='P' && cor=="Preto") || (vez=='B' && cor=="Branco")) {
-              celula.onclick = ((fun, posl, posc) => {
-               return () => fun(posl, posc);
-             })(this.play.bind(this), l, c);
-           }
+            celula.onclick = ((fun, posl, posc) => {
+              return () => fun(posl, posc);
+              console.log(this.conteudo);
+            })(this.play.bind(this), l, c);
+           
          }
        }
        console.log(this.conteudo);
