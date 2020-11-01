3d6353c4dea08d5a7674a1fd6d2e930ed713910e
carolnicolau <62442865+carolnicolau@users.noreply.github.com>
2020-11-01 20:06:55 +0000

    Add files via upload

--- game.js
+++ game.js
@@ -35,7 +35,6 @@ window.onload = function() {
   //FIM DA NAVBAR
 
   var oponente1="";
-  var vez1 = 'P';
   var cor1="Preto";
   var dificuldade1="";
 
@@ -71,42 +70,82 @@ window.onload = function() {
   
   //ACCIONAR BOTÃO DE INICIAR
   document.getElementById("iniciar").onclick = function() {
-    escondeEsconde();
-    jogo = new Jogo(cor1);
-    mostraMostra();
-  }
-
-  
-
-  
+      jogo = new Jogo(cor1);
+      escondeEsconde();
+    
+  }  
 
   class Jogo { 
 
-    soma_pecas() {
-      let conta_pretas=0;
-      let conta_brancas=0;
-      let conta_livres=0;
+    soma_pecas(p, b, l) {
+      p=0;
+      b=0;
+      l=0;
 
       for(let l = 0; l<8; l++) {
         for(let c=0; c<8; c++) {
           if(this.conteudo[l][c] == 'B')
-            conta_brancas ++;
+            b ++;
           else if(this.conteudo[l][c] == 'P')
-            conta_pretas ++;
+            p ++;
           else
-            conta_livres ++;
+            l ++;
         }
       }
-      document.getElementById("n_brancas").innerText = conta_brancas;
-      document.getElementById("n_pretas").innerText = conta_pretas;
-      document.getElementById("n_livres").innerText = conta_livres;
+      document.getElementById("n_brancas").innerText = b;
+      document.getElementById("n_pretas").innerText = p;
+      document.getElementById("n_livres").innerText = l;
+      return b + p;
     }
 
     computador() {
       //var jogadas_legais = jogo.calcular_legais(vez, jogo.conteudo);
-      this.play(1,1);
+      this.play(0,0);
       this.MudarDeVez();
     }
+
+    humano(l, c) {
+      if(!this.vez_computador()) {
+        if(this.jogadas_legais[l][c] == this.vez){ //verificação passa para jogada válida
+          this.play(l, c);
+          this.MudarDeVez();
+        }
+      } else {
+        alert("Não é a tua vez!");
+      }
+    }
+
+    play(l, c) {
+      var peca1 = this.tabuleiro[l][c].firstChild;
+        
+      if(this.vez =='P') {
+        peca1.className = "peca preto";
+        this.conteudo[l][c] = 'P';
+        console.log("Preto jogou!");
+      } else {
+        peca1.className = "peca branco";
+        this.conteudo[l][c] = 'B';
+        console.log("Branco jogou!");
+      }
+
+      this.flip(l,c);
+
+      var peca2;
+      for(let l=0; l<8; l++) {
+        for(let c=0; c<8; c++) {
+          peca2 = this.tabuleiro[l][c].firstChild;
+
+          if(this.vez == 'P'){
+            peca2.classList.remove("validas_branco");
+            //console.log("remove válida branca: ("+l+","+c+")");
+          }
+          else if(this.vez == 'B')
+            peca2.classList.remove("validas_preto");
+            //console.log("remove válida preta: ("+l+","+c+")");
+        }
+      }
+    }
+
 
     vez_computador() {
       if((this.cor=="Branco" && this.vez=='P') || (this.cor=="Preto" && this.vez=='B')) {
@@ -120,7 +159,38 @@ window.onload = function() {
       return oposta;
     }
 
+    terminar() {
+      console.log("terminando");
+      if(this.desistiu)
+        this.vencedor = "computador";
+      else {
+        var p;
+        var b; 
+        var l;
+        soma_pecas(p,b,l);
+
+        if((p>b && cor == 'P') || (p<b && cor == 'B' ))
+          this.vencedor = "humano";
+        else if((p<b && cor == 'P') || (p>b && cor == 'B' ))
+          this.vencedor = "computador";
+        else
+          this.vencedor = "empate";
+      }
+      alert("vencedor: "+ this.vencedor);
+      location.reload();
+    }
+
     MudarDeVez(){
+
+      if(this.terminou() || this.desistiu) {
+        oponente1="";
+        cor1="Preto";
+        dificuldade1="";
+        mostraMostra();
+        terminar();
+        return;
+      }
+
       if(this.vez == 'B'){
         this.vez='P';
         console.log("vez do preto");
@@ -128,29 +198,41 @@ window.onload = function() {
       else if(this.vez=='P'){
         this.vez='B';
         console.log("vez do branco");
-      }
+      }    
 
       console.log("já no tabuleiro:");
       for(let l=0; l<8; l++) {
         for(let c=0; c<8; c++) {
-          if(this.conteudo[l][c] == vez) {
+          if(this.conteudo[l][c] == this.vez) {
             console.log("\t("+l+","+c+")");
           }
         }
       }
-      
+
+
       this.jogadas_legais   = this.calcular_legais(this.vez);
       var n_jogadas_vez     = this.count_legais(this.jogadas_legais);
-      
+      var peca1;
+
       console.log("jogadas legais de vez: " + n_jogadas_vez);
       console.log(this.jogadas_legais); 
+      
+
       for(let l=0; l<8; l++) {
         for(let c=0; c<8; c++) {
+          peca1 = this.tabuleiro[l][c].firstChild;
+          
           if(this.jogadas_legais[l][c] == this.vez) {
+            if(this.vez=='B') {
+              peca1.className += " validas_branco";
+            }
+            else if (this.vez=='P') {
+              peca1.className += " validas_preto";
+            }
             console.log("\t("+l+","+c+")");
           }
         }
-      }
+      }  
 
       if(this.vez_computador()) {
         this.computador();
@@ -158,11 +240,69 @@ window.onload = function() {
     }
 
     desistir() {
+      this.desistiu = true;
       alert("ganhou alguém");
+      this.MudarDeVez();
     }
 
+    terminou() {
+      if(this.soma_pecas()==64) {
+        alert("jogo acabou!");
+        return true;
+      }
+      else
+        return false;
+    }
     
   /****************************************************************************/
+    flip_celula(l,c) {
+
+      this.conteudo[l][c] = this.vez;
+          let peca = this.tabuleiro[l][c].firstChild;
+          
+          if(this.vez=='P')
+            peca.className = "peca preto";
+          else
+            peca.className = "peca branco";
+    }
+
+    flip_linha(dl, dc, l, c) {     
+      if((l+dl < 0) || (l+dl > 7)) {
+        return false;
+      }
+      if((c+dc < 0) || (c+dc > 7)) {
+        return false;
+      }
+      if(this.conteudo[l+dl][c+dc] == ' ') {
+        return false;
+      }
+      if(this.conteudo[l+dl][c+dc] == this.vez) { 
+        return true;
+      }
+      else {
+        if(this.flip_linha(dl,dc,l+dl,c+dc)) {
+          this.flip_celula(l+dl,c+dc);
+          return true;
+        }
+        else {
+          return false;
+        }
+      }
+    }
+
+    flip(l,c) {
+      this.flip_linha(-1, -1, l, c);
+      this.flip_linha(-1,  0, l, c);
+      this.flip_linha(-1,  1, l, c);
+            
+      this.flip_linha(1, -1, l, c);
+      this.flip_linha(1,  0, l, c);
+      this.flip_linha(1,  1, l, c);
+          
+      this.flip_linha(0, -1, l, c);
+      this.flip_linha(0,  1, l, c);
+    }
+
     verifica_linha(v, dl, dc, l, c) {
       //verifica se há uma cor=vez algures na linha (l,c)+d(dl,dc)
       if(this.conteudo[l][c] == v)
@@ -182,7 +322,9 @@ window.onload = function() {
     is_legal(v, dl, dc, l, c) {
       //verifica se a posição adjacente a l,c têm cor oposta a vez 
       //e se a reta (l,c)+d(dl,dc) termina em cor=vez
-      let oposta = this.oposto_vez();
+      
+      //let oposta = this.oposto_vez();
+      let oposta = (v=='B')? 'P' : 'B';
       
       if((l+dl < 0) || (l+dl > 7)) {
         return false;
@@ -190,7 +332,7 @@ window.onload = function() {
       if((c+dc < 0) || (c+dc > 7)) { //celula adjacente direta está fora do tabuleiro
         return false;
       }
-      if(this.conteudo[l+dl][c+dc] != oposta) { //celula adjacente direta é da cor oposta
+      if(this.conteudo[l+dl][c+dc] != oposta) { //celula adjacente direta tem de ser da cor oposta
         return false;
       }
       if((l+dl+dl < 0) || (l+dl+dl > 7)) { //celulas proximas estão fora do tabuleiro
@@ -209,21 +351,24 @@ window.onload = function() {
         legais[l] = new Array(8);
         
         for(let c=0; c<8; c++) {
-          legais[l][c] = ' ';
 
-          let nw = this.is_legal(v, -1, -1, l, c);
-          let nn = this.is_legal(v, -1,  0, l, c);
-          let ne = this.is_legal(v, -1,  1, l, c);
-          
-          let sw = this.is_legal(v, 1, -1, l, c);
-          let ss = this.is_legal(v, 1,  0, l, c);
-          let se = this.is_legal(v, 1,  1, l, c);
-          
-          let ww = this.is_legal(v, 0, -1, l, c);
-          let ee = this.is_legal(v, 0,  1, l, c);
+          if(this.conteudo[l][c] == ' ') {
+            legais[l][c] = ' ';
 
-          if(nw || nn || ne || sw || ss || se || ww || ee) {
-            legais[l][c] = v; 
+            let nw = this.is_legal(v, -1, -1, l, c);
+            let nn = this.is_legal(v, -1,  0, l, c);
+            let ne = this.is_legal(v, -1,  1, l, c);
+            
+            let sw = this.is_legal(v, 1, -1, l, c);
+            let ss = this.is_legal(v, 1,  0, l, c);
+            let se = this.is_legal(v, 1,  1, l, c);
+            
+            let ww = this.is_legal(v, 0, -1, l, c);
+            let ee = this.is_legal(v, 0,  1, l, c);
+
+            if(nw || nn || ne || sw || ss || se || ww || ee) {
+              legais[l][c] = v; 
+            }
           }
         }
       }
@@ -242,44 +387,18 @@ window.onload = function() {
       return count;
     }
 
-    humano(l, c) {
-      if(!this.vez_computador()) {
-        this.play(l, c);
-        this.MudarDeVez();
-      } else {
-        alert("Não é a tua vez!");
-      }
-    }
 
-    play(l, c) {
-      //this.jogadas_legais = this.calcular_legais();
-
-      var peca1 = this.tabuleiro[l][c].firstChild;
-        
-      if(this.conteudo[l][c] == ' '){ //verificação passa para jogada válida
-        if(this.vez =='P') {
-          peca1.className = "peca preto";
-          this.conteudo[l][c] = 'P';
-          console.log("Preto jogou!");
-        } else {
-          peca1.className = "peca branco";
-          this.conteudo[l][c] = 'B';
-          console.log("Branco jogou!");
-        }
-        this.soma_pecas();
-      }
-    }
 
     constructor(cor) {
 
       if (!Jogo.instancia) {
-        Jogo.instancia = this;
-
         this.vez = 'B';
         this.cor = cor;
         this.conteudo = new Array(8);
         this.jogadas_legais;
         this.tabuleiro = new Array(8);
+        this.desistiu = false;
+        this.vencedor = "";
 
         const base = document.getElementById("base");
         const tabul = document.createElement("div");
@@ -292,9 +411,9 @@ window.onload = function() {
         desistir.innerText = "Desistir";
         desistir.id = "desistir";
 
-        desistir.onclick = function() {
-          mostraMostra();
-        };
+        desistir.onclick = ((fun) => {
+              return () => fun();
+            })(this.desistir.bind(this));
          
         base.appendChild(tabul);
         base.appendChild(passar);
@@ -345,15 +464,11 @@ window.onload = function() {
        }
        console.log(this.conteudo);
        console.log(this.tabuleiro);
+        this.MudarDeVez();
+        Jogo.instancia = this;
      }
-    
-    this.MudarDeVez();
     
     return Jogo.instancia;
    }
-   //////////????????????
-   static getInstancia() {
-    return this._instance;
-  }
-} 
+}
 }
