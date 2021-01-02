4d5247f5ace0e51dc77b6eada3eae51fcc5be526
carolnicolau <62442865+carolnicolau@users.noreply.github.com>
2021-01-02 13:04:23 +0000

    notify

--- paginas/flip.js
+++ paginas/flip.js
@@ -2,11 +2,10 @@
 //o booleano simulação indica se esta jogada l,c está a acontecer dentro de um minimax
 //para que, caso seja verdadeiro, não sejam modificados elementos html
 
-
 function flip_celula(l,c,conteudo_, vez_, simulacao) {
 
   conteudo_[l][c] = vez_;
-  
+
   if(!simulacao) {
     let jogo = Jogo.getInstancia();
     let peca = jogo.tabuleiro[l][c].firstChild;
@@ -23,11 +22,50 @@ function flip_celula(l,c,conteudo_, vez_, simulacao) {
   }
 }
 
+function flip_linha(dl, dc, l, c, conteudo_, vez_, simulacao) {
+  if((l+dl < 0) || (l+dl > 7)) {
+    return false;
+  }
+  if((c+dc < 0) || (c+dc > 7)) {
+    return false;
+  }
+  if(conteudo_[l+dl][c+dc] == ' ') {
+    return false;
+  }
+  if(conteudo_[l+dl][c+dc] == vez_) {
+    return true;
+  }
+  else {
+    if(flip_linha(dl,dc,l+dl,c+dc,conteudo_,vez_,simulacao)) {
+      flip_celula(l+dl,c+dc,conteudo_,vez_,simulacao);
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+}
+
+function flip(l,c,conteudo_,vez_,simulacao) {
+
+  flip_linha(-1, -1, l, c,conteudo_,vez_,simulacao);
+  flip_linha(-1,  0, l, c,conteudo_,vez_,simulacao);
+  flip_linha(-1,  1, l, c,conteudo_,vez_,simulacao);
+
+  flip_linha(1, -1, l, c,conteudo_,vez_,simulacao);
+  flip_linha(1,  0, l, c,conteudo_,vez_,simulacao);
+  flip_linha(1,  1, l, c,conteudo_,vez_,simulacao);
+
+  flip_linha(0, -1, l, c,conteudo_,vez_,simulacao);
+  flip_linha(0,  1, l, c,conteudo_,vez_,simulacao);
+}
+
+
 /*
 async function flip_celula(l,c,conteudo_, vez_, simulacao) {
 
   conteudo_[l][c] = vez_;
-  
+
   if(!simulacao) {
     let jogo = Jogo.getInstancia();
     let peca = jogo.tabuleiro[l][c].firstChild;
@@ -56,41 +94,3 @@ async function flip_celula(l,c,conteudo_, vez_, simulacao) {
   }
 }
 */
-
-function flip_linha(dl, dc, l, c, conteudo_, vez_, simulacao) {     
-  if((l+dl < 0) || (l+dl > 7)) {
-    return false;
-  }
-  if((c+dc < 0) || (c+dc > 7)) {
-    return false;
-  }
-  if(conteudo_[l+dl][c+dc] == ' ') {
-    return false;
-  }
-  if(conteudo_[l+dl][c+dc] == vez_) { 
-    return true;
-  }
-  else {
-    if(flip_linha(dl,dc,l+dl,c+dc,conteudo_,vez_,simulacao)) {
-      flip_celula(l+dl,c+dc,conteudo_,vez_,simulacao);
-      return true;
-    }
-    else {
-      return false;
-    }
-  }
-}
-
-function flip(l,c,conteudo_,vez_,simulacao) {
-
-  flip_linha(-1, -1, l, c,conteudo_,vez_,simulacao);
-  flip_linha(-1,  0, l, c,conteudo_,vez_,simulacao);
-  flip_linha(-1,  1, l, c,conteudo_,vez_,simulacao);
-
-  flip_linha(1, -1, l, c,conteudo_,vez_,simulacao);
-  flip_linha(1,  0, l, c,conteudo_,vez_,simulacao);
-  flip_linha(1,  1, l, c,conteudo_,vez_,simulacao);
-
-  flip_linha(0, -1, l, c,conteudo_,vez_,simulacao);
-  flip_linha(0,  1, l, c,conteudo_,vez_,simulacao);
-}
